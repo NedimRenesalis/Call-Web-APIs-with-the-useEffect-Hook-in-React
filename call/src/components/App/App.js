16d8c8700b879727db4d8e56b4164c073cba6b64
Nedim Begić <nedim@renesalis-packaging.com>
2021-04-25 23:10:06 +0200

    import getList

--- call/src/components/App/App.js
+++ call/src/components/App/App.js
@@ -1,8 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import { getList } from '../../services/list';
 
 function App() {
   const [list, setList] = useState([]);
+
+  useEffect(() => {
+   let mounted = true;
+   getList()
+     .then(items => {
+       if(mounted) {
+         setList(items)
+       }
+     })
+   return () => mounted = false;
+ }, [])
+
   return(
     <>
     </>
