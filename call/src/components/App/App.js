e7552d0225cff349c1b6ceed027aad9b05495d93
Nedim Begić <nedim@renesalis-packaging.com>
2021-04-25 23:55:50 +0200

    Submitting in the form to POST to API

--- call/src/components/App/App.js
+++ call/src/components/App/App.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import { getList } from '../../services/list';
+import { getList, setItem } from '../../services/list';
 
 function App() {
+ const [alert, setAlert] = useState(false);
  const [itemInput, setItemInput] = useState('');
  const [list, setList] = useState([]);
 
@@ -17,13 +18,31 @@ function App() {
     return () => mounted = false;
   }, [])
 
+ useEffect(() => {
+    if(alert) {
+      setTimeout(() => {
+        setAlert(false);
+      }, 1000)
+    }
+  }, [alert])
+
+ const handleSubmit = (e) => {
+    e.preventDefault();
+    setItem(itemInput)
+    .then(() => {
+        setItemInput('');
+        setAlert(true);
+      })
+  };
+
   return(
     <div className="wrapper">
      <h1>My Grocery List</h1>
      <ul>
        {list.map(item => <li key={item.item}>{item.item}</li>)}
      </ul>
-     <form>
+     {alert && <h2> Submit Successful</h2>}  
+   <form onSubmit={handleSubmit}>
        <label>
          <p>New Item</p>
          <input type="text"onChange={event => setItemInput(event.target.value)} value={itemInput} />
